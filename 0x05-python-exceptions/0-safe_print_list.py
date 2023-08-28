c30d19b13cc4363f33a9c0d864d1d4e6ca33a1f6
MphoCM <christian.mofokeng@gmai.com>
2023-08-28 12:23:35 -0700

    updated

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    """function that prints x elements of a list."""
+    real_number_of_elements = 0
+    for variable in range(x):
+        try:
+            print("{}".format(my_list[variable]), end="")
+            real_number_of_elements += 1
+        except Exception:
+            break
+    print("")
+    return (real_number_of_elements)        
