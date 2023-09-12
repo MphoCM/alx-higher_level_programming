805ccb680c88eabb2b38caaa8aa60b36a3269d44
MphoCM <christian.mofokeng@gmai.com>
2023-09-12 12:03:04 -0700

    updated

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module to a list of lists of integers representingthe Pascals triangle of n:
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+       representing the Pascal triangle of n
+    Args: n ()
+    """
+    if n <= 0:
+        return []
+    tri = []
+
+    for row in range(n):
+        curr_row = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                curr_row.append(1)
+            else:
+                curr_row.append(tri[row - 1][col - 1] + tri[row - 1][col])
+        tri.append(curr_row)
+    return tri
