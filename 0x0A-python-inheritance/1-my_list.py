067c9a8b3f9a146511b358512380f43b2f5fed3a
MphoCM <christian.mofokeng@gmai.com>
2023-09-13 12:01:44 -0700

    update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-"""This module is Write a class MyList that inherits from list,
-contains a afunction prints the list, but sorted (ascending sort)
+"""
+Below class object inherits from
+a list and prints a sorted version
+of the list
 """
 
 
 class MyList(list):
-    """
-    Define a class named MyList
-    Inherits from: list
-    has Public instance method: def print_sorted(self)
-
-    Examples:
-    >>> import doctest
-    >>> doctest.testfile('1-my_list.txt')
-    """
-# self: reference to the instance of the MyList class.
+    """Created class object."""
     def print_sorted(self):
-        """method takes self as arg
-        Args: self
-        Returns : void : print sorted list"""
-        new_list = sorted(self)
-        print(new_list)
+        """prints sorted list."""
+        print(sorted(self))
