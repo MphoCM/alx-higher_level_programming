dc9f4bfb6160cfc7ac8ca9134c4123ab83f38155
MphoCM <christian.mofokeng@gmail.com>
2023-10-17 14:55:03 -0700

    update

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- script removes records witha score <= 5 n second_table
+-- database hbtn_0c_0 in your MySQL
+
+DELETE
+FROM second_table
+WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- a script that computes the score average of all records in the table second_table
+-- the result column is average
+
+SELECT
+AVG(score) AS average
+FROM second_table;
