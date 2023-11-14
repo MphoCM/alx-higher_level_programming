91f5236245a0e5e8b9ca5317183bb3c94c94fdfa
MphoCM <christian.mofokeng@gmail.com>
2023-11-14 08:33:45 -0800

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
-switch (process.argv.length) {
-	case 2:
-    console.log('No argument');
-    break;
-  case 3:
-    console.log('Argument found');
-    break;
-
-  default:
-    console.log('Arguments found');
-    break;
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
