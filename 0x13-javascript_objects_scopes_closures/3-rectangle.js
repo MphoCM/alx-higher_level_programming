f171fcf4278e14829961199e84372bbc06ac3bea
MphoCM <christian.mofokeng@gmail.com>
2023-11-14 11:42:40 -0800

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
-#!/usr/bn/node
+#!/usr/bin/node
 
-class Rectang {
-	constructor (w, h) {
+class Rectangle {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
