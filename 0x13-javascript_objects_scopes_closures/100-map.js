e8f2e410d9bbc54c89309a536efb8f988e46cb27
MphoCM <christian.mofokeng@gmail.com>
2023-11-15 11:09:13 -0800

    100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
 
-console.log(list);
-console.log(newList);
+const array = require('./100-data').list;
+const newArray = array.map((x, i) => x * i);
+console.log(array);
+console.log(newArray);
