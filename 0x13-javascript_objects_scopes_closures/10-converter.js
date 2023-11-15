063181d62fdbd25617a608843d5b839f52399c9a
MphoCM <christian.mofokeng@gmail.com>
2023-11-15 11:04:26 -0800

    update

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
-#!usr/bin/node
+#!/usr/bin/node
 
-exports.converter = fucntion (base) {
-	return function (number) {
-    return number.toString(base);
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
   };
 };
