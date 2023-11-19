c7eb47377961aac8eca49fa7ef297b3839e50f39
MphoCM <christian.mofokeng@gmail.com>
2023-11-19 04:29:19 -0800

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-"""a script that takes in arguments and
-dipslaysall vlues in the states table of hbtn_0e_0_usa
-where name matches the argument.
-safe from MySQL injections!"""
+"""
+Import modules for script.
+"""
+import sys
+import MySQLdb
 
 
-improt sys
-iport MySQLdb
+def main(args):
+    """
+    Script that is safe from sql injections.
+    """
+    if len(args) != 5:
+        raise Exception("4 arguments needed!")
+    db = MySQLdb.connect(
+            host='localhost',
+            user=args[1],
+            passwd=args[2],
+            db=args[3]
+            )
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+            (args[4],))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            user=sys.argv[1], passwed=sys.argv[2], db=sys.argv[3], port=3306)
-
-    cur = db.cursor()
-    stateName = sys.argv[4]
-    cur.execute(
-            "SELECT * FROM states WHERE name = % ORDER BY id ASC;", (stateName,))
-
-    state = cur.fetchall()
-    for state in state:
-        print(state)
+    main(sys.argv)
