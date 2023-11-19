fb71e287055941a14ae2dd2f7140f3aa208291c0
MphoCM <christian.mofokeng@gmail.com>
2023-11-19 04:43:32 -0800

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,18 +6,23 @@ that contain the letter a from database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create-engine
+from sqlalchemy import create_engine
+from sqlalchemy import MetData
 from sqlalchemy.orm import sessionmaker
 
-# should not be executed when imported
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    a_states = session.query(State).filter(
-        State.name.like('%a%')).order_by(State.id).all()
 
-    for state in a_states:
-        print("{}: {}".format(state.id, state.name))
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+    state = sesh.query(State).filter(
+            State.name.contains('a')).order_by(State.id).all()
+    for place in state:
+        print("{}: {}".format(place.id, place.name))
+    sesh.close()
