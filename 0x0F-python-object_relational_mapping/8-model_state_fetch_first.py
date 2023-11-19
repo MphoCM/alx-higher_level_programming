01eef0eedcfdb3a4ccacbea80717adc82c8d3aee
MphoCM <christian.mofokeng@gmail.com>
2023-11-19 04:39:16 -0800

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,18 +5,23 @@ Lists all State objects from the database hbtn_0e_6_usa
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
 
-# should not be executed when imported
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    first_state = session.query(State).first()
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+    state = sesh.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print('Nothing')
+        print("Nothing")
+    sesh.close()
