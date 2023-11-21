1b2f5f5ee597ee466091dd094018ccade6948f32
MphoCM <christian.mofokeng@gmail.com>
2023-11-21 11:35:46 -0800

    100-relationship_states_city.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_city.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_city.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""a python file that contains the class definition of a State
-and an instance Base = declarative_base()"""
+"""
+Creates the State "California" with the City "San Francisco" from a DB
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-metadata = MetaData()
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-class City(Base):
-    """class City inherits from Base"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
+
+    session.add(newState)
+    session.add(newCity)
+    session.commit()
