3a7b1e5a570b89a6011273dfdf86b1b01e35cee5
MphoCM <christian.mofokeng@gmail.com>
2024-01-03 12:01:06 -0800

    updating

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
-const file = process.argv[2];
-const string = process.argv[3];
 const fs = require('fs');
-fs.writeFile(file, string, function (err) {
+const path = process.argv[2];
+const string = process.argv[3];
+fs.writeFile(path, string, function (err) {
   if (err) {
     console.log(err);
   }
