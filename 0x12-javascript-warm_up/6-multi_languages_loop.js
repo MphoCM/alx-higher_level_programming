e8672e1fbd2c59988623770f2ba0aa32e97e8cb6
MphoCM <christian.mofokeng@gmail.com>
2024-02-03 07:59:45 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (et index = 0; index < lines.length; index++) {
-	console.log(lines[index]);
+for (const x of lines) {
+  console.log(x);
 }
