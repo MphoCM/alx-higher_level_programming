adb68633638abbdbfcc228ccd51efba8027bb937
MphoCM <christian.mofokeng@gmail.com>
2024-02-03 08:10:36 -0800

    102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-exports.addMeMaybe = function (number, theFunction) {
-  theFunction(++number);
+function addMeMaybe (number, theFunction) {
+  number += 1;
+  theFunction(number);
 }
+module.exports = {
+  addMeMaybe
+};
